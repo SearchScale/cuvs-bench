10f20ba8a5f4dafbb7811ccf75695ef305fbc011
noblepaul <noble.paul@gmail.com>
2024-11-14 22:10:40 +1100

    scripts to run tests

--- bench.sh
+++ bench.sh
@@ -65,8 +65,8 @@ curl -X POST -H "Content-Type: application/json" \
 
 #java -cp ./target/solr-cuvs-benchmarks-1.0-SNAPSHOT.jar:./target/solr-cuvs-benchmarks-1.0-SNAPSHOT-jar-with-dependencies.jar com.searchscale.benchmarks.SolrBenchmark /data/wikipedia_vector_dump.csv.gz true -1 100000 3
 
-#generate javabin
-#java -cp ./target/solr-cuvs-benchmarks-1.0-SNAPSHOT.jar:./target/solr-cuvs-benchmarks-1.0-SNAPSHOT-jar-with-dependencies.jar com.searchscale.benchmarks.Indexer /data/wikipedia_vector_dump.csv.gz  /data/250k.javabin 1000000 260000
+#generate javabin last param true=legacy
+#java -cp ./target/solr-cuvs-benchmarks-1.0-SNAPSHOT.jar:./target/solr-cuvs-benchmarks-1.0-SNAPSHOT-jar-with-dependencies.jar com.searchscale.benchmarks.Indexer /data/wikipedia_vector_dump.csv.gz  /data/javabin/100k.javabin 1000000 100000 true
 
 #POST javabin payload to solr
 # time curl -X POST --data-binary "@200k.javabin" -H "Content-Type: application/javabin" "http://localhost:8983/solr/test/directupdate?commit=true"
